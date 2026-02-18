d277c0c3c56479bea60d0a35e712466876f6de62
Kesaa89 <192440616+Kesaa89@users.noreply.github.com>
2026-02-18 21:12:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,8 @@
-const CACHE_NAME = 'raspored-v5';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
-];
-
-// Инсталација и кеширање фајлова
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
   self.skipWaiting();
 });
 
-// Активација и брисање старе кеш меморије
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch стратегија: Прво мрежа, ако не успе онда кеш
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
+  // Ово омогућава апликацији да ради нормално преко мреже
+  e.respondWith(fetch(e.request));
 });
